3a4a7565eed1c9495ce68381a034a2a6eeaebc99
Dmytro Palamarchuk <moonbrv@gmail.com>
2018-01-20 17:01:36 +0200

    add ball basic movements

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,173 @@
 import './reset.css'
 import './main.css'
+
+enum ColorPalette {
+  Black = '#000000',
+  Cyan = '#009688',
+  Gray = '#6A6A6A',
+  White = '#E8E1E1',
+  Orange = '#FB5521'
+}
+
+enum Size {
+  Ball = 10
+}
+
+// const colors = {
+//   black: '000000',
+//   cyan: '#009688',
+//   gray: '#6A6A6A',
+//   white: '#E8E1E1',
+//   orange: '#FB5521'
+// }
+
+const gameCanvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('game')
+
+class Vector {
+  private _x: number
+  private _y: number
+  constructor (x = 0, y = 0) {
+    this._x = x
+    this._y = y
+  }
+  get x(): number {
+    return this._x
+  }
+  set x(newX: number) {
+    this._x = newX
+  }
+  get y(): number {
+    return this._y
+  }
+  set y(newY: number) {
+    this._y = newY
+  }
+}
+
+class Rectangle {
+  private _position: Vector
+  private _size: Vector
+
+  constructor (w: number = 0, h: number = 0) {
+    this._position = new Vector()
+    this._size = new Vector(w, h)
+  }
+
+  get position(): Vector {
+    return this._position
+  }
+
+  get size(): Vector {
+    return this._size
+  }
+
+  get left(): number {
+    return this.position.x
+  }
+
+  get right(): number {
+    return this.position.x + this.size.x
+  }
+
+  get top(): number {
+    return this.position.y + this.size.y
+  }
+
+  get bottom(): number {
+    return this.position.y
+  }
+}
+
+class Ball extends Rectangle {
+  private _velocity: Vector
+
+  constructor () {
+    super(Size.Ball, Size.Ball)
+    this._velocity = new Vector()
+  }
+
+  get velocity(): Vector {
+    return this._velocity
+  }
+
+  updatePosition (dt: number): void {
+    this.position.x = this.position.x + this.velocity.x * dt
+    this.position.y = this.position.y + this.velocity.y * dt
+  }
+}
+
+class PingPongGame {
+  private _canvas: HTMLCanvasElement
+  private _context: CanvasRenderingContext2D
+  private _ball: Ball
+  private _lastUpdateTime: number
+
+  constructor (canvas: HTMLCanvasElement) {
+    this._canvas = canvas
+    this._context = canvas.getContext('2d')
+    this._ball = new Ball()
+    this._lastUpdateTime = 0
+    this.ball.velocity.x = 2
+    this.ball.velocity.y = 2
+  }
+
+  get canvas(): HTMLCanvasElement {
+    return this._canvas
+  }
+
+  get context(): CanvasRenderingContext2D {
+    return this._context
+  }
+
+  get ball(): Ball {
+    return this._ball
+  }
+
+  get lastUpdateTime(): number {
+    return this._lastUpdateTime
+  }
+
+  set lastUpdateTime(miliseconds: number) {
+    this._lastUpdateTime = miliseconds
+  }
+
+  updateCanvas ():void {
+    this.context.fillStyle = ColorPalette.Black
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
+  }
+
+  drawRectangle (rect: Rectangle, color: ColorPalette = ColorPalette.White): void {
+    this.context.fillStyle = color
+    this.context.fillRect(rect.position.x, rect.position.y, rect.size.x, rect.size.y)
+  }
+
+  checkBallMoveDirection (ball: Ball):void {
+    if (ball.left <= 0 || ball.right >= this.canvas.width) {
+      ball.velocity.x = -ball.velocity.x
+    }
+    if (ball.bottom <= 0 || ball.top >= this.canvas.height) {
+      ball.velocity.y = -ball.velocity.y
+    }
+  }
+
+  updateElementsPositions = (dt: number): void => {
+    this.ball.updatePosition(dt)
+
+    this.checkBallMoveDirection(this.ball)
+
+    this.updateCanvas()
+    this.drawRectangle(this.ball)
+  }
+
+  updateGame = (miliseconds: number): void => {
+    if (this.lastUpdateTime) {
+      const dt = (miliseconds - this.lastUpdateTime) / 10
+      this.updateElementsPositions(dt)
+    }
+    this.lastUpdateTime = miliseconds
+    requestAnimationFrame(this.updateGame)
+  }
+}
+
+const game = new PingPongGame(gameCanvas)
+game.updateGame(0)
